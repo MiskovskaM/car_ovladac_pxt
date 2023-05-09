ccf9ef0dd4ec16713aa01a74a7c712717b2006e0
MiskovskaM <115079420+MiskovskaM@users.noreply.github.com>
2023-05-10 00:15:04 +0200

    aaa

--- main.ts
+++ main.ts
@@ -8,6 +8,25 @@ radio.setFrequencyBand(6)
 radio.sendNumber(5)
 radio.sendString("jeď")
 
+let leftIdx = 0
+let rightInx = 0
+const speeds = [0, 30, 50, 70, 85, 100]
+input.onButtonPressed(Button.A, function () {
+    PCAmotor.MotorRun(PCAmotor.Motors.M1,
+        speeds[++leftIdx % speeds.length])
+    basic.showNumber(speeds[leftIdx])
+    basic.clearScreen()
+})
 
+input.onButtonPressed(Button.B, function () {
+    PCAmotor.MotorRun(PCAmotor.Motors.M4,
+        speeds[++rightInx % speeds.length])
+    basic.showNumber(speeds[rightInx])
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    PCAmotor.MotorStopAll()
+    leftIdx = rightInx = 0
+})
 
 
