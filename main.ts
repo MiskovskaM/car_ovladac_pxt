2b2fe069d9e4142202eb0d9939441f5f0ce8eb4a
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-11 20:18:29 +0200

    ovladame to tlacitky"!!!!!

--- main.ts
+++ main.ts
@@ -1,32 +1,36 @@
-//servo S1
-//levý motor M1
-//pravý motor M4
-//ovladač
-radio.setGroup(22)
-radio.setFrequencyBand(6)
+//ovladani tlacitky
+let lastBtnA = 0;
+let lastBtnB = 0;
+let lastLogo = 0;
+let lastP2 = 0;
 
-radio.sendNumber(5)
-radio.sendString("jeď")
+function sendData() {
+    let data = {
+        x: input.rotation(Rotation.Pitch),
+        y: input.rotation(Rotation.Roll),
+        a: input.buttonIsPressed(Button.A),
+        b: input.buttonIsPressed(Button.B),
+        l: input.logoIsPressed(),
+        p0: input.pinIsPressed(TouchPin.P0),
+        p1: input.pinIsPressed(TouchPin.P1),
+        p2: input.pinIsPressed(TouchPin.P2)
+    }
+    let posliData = String.fromCharCode(data.x) + String.fromCharCode(data.y) + (data.a ? 1 : 0) + (data.b ? 1 : 0) + (data.l ? 1 : 0) + (data.p0 ? 1 : 0) + (data.p1 ? 1 : 0) + (data.p2 ? 1 : 0);
+    radio.sendString(posliData);
+}
 
-let leftIdx = 0
-let rightInx = 0
-const speeds = [0, 30, 50, 70, 85, 100]
-input.onButtonPressed(Button.A, function () {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1,
-        speeds[++leftIdx % speeds.length])
-    basic.showNumber(speeds[leftIdx])
-    basic.clearScreen()
+basic.forever(function () {
+    let btnA = input.buttonIsPressed(Button.A);
+    let btnB = input.buttonIsPressed(Button.B);
+    let logo = input.logoIsPressed();
+    let p2 = input.pinIsPressed(TouchPin.P2);
+
+    if (+btnA != lastBtnA || +btnB != lastBtnB || +logo != lastLogo || +p2 != lastP2) {
+        sendData();
+    }
+
+    lastBtnA = +btnA;
+    lastBtnB = +btnB;
+    lastLogo = +logo;
+    lastP2 = +p2;
 })
-
-input.onButtonPressed(Button.B, function () {
-    PCAmotor.MotorRun(PCAmotor.Motors.M4,
-        speeds[++rightInx % speeds.length])
-    basic.showNumber(speeds[rightInx])
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.AB, function () {
-    PCAmotor.MotorStopAll()
-    leftIdx = rightInx = 0
-})
-
-
