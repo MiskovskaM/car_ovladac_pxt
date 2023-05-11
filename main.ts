6fc07ee256a1d69428809c8b8b31e96a40069c89
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-11 10:53:24 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,33 @@
-<!-- //servo S1
+//servo S1
 //levý motor M1
 //pravý motor M4
 //ovladač
-radio.setGroup(22)
-radio.setFrequencyBand(6)
+//radio.setGroup(22)
+//radio.setFrequencyBand(6)
 
-radio.sendNumber(5)
-radio.sendString("jeď")
+//radio.sendNumber(5)
+//radio.sendString("jeď")
 
-let leftIdx = 0
-let rightInx = 0
-const speeds = [0, 30, 50, 70, 85, 100]
-input.onButtonPressed(Button.A, function () {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1,
-        speeds[++leftIdx % speeds.length])
-    basic.showNumber(speeds[leftIdx])
-    basic.clearScreen()
-})
+//let leftIdx = 0
+//let rightInx = 0
+//const speeds = [0, 30, 50, 70, 85, 100]
+//input.onButtonPressed(Button.A, function () {
+//    PCAmotor.MotorRun(PCAmotor.Motors.M1,
+//        speeds[++leftIdx % speeds.length])
+//    basic.showNumber(speeds[leftIdx])
+//    basic.clearScreen()
+//})
 
-input.onButtonPressed(Button.B, function () {
-    PCAmotor.MotorRun(PCAmotor.Motors.M4,
-        speeds[++rightInx % speeds.length])
-    basic.showNumber(speeds[rightInx])
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.AB, function () {
-    PCAmotor.MotorStopAll()
-    leftIdx = rightInx = 0
-}) -->
+//input.onButtonPressed(Button.B, function () {
+//    PCAmotor.MotorRun(PCAmotor.Motors.M4,
+//        speeds[++rightInx % speeds.length])
+//   basic.showNumber(speeds[rightInx])
+//    basic.clearScreen()
+//})
+//input.onButtonPressed(Button.AB, function () {
+//    PCAmotor.MotorStopAll()
+//    leftIdx = rightInx = 0
+//})
 
 
 
