8f9d6237c7a0fc13a4fac2a8d95df6588f165684
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-19 13:27:49 +0200

    jezdime na osy

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 //ovladani tlacitky
-let lastOsaX = 0;
-let lastOsaY = 0;
+radio.setGroup(54);
+radio.setFrequencyBand(7);
+
+let lastX = 0;
+let lastY = 0;
 let lastBtnA = 0;
 let lastBtnB = 0;
 let lastLogo = 0;
-let lastPinP0 = 0;
-let lastPinP1 = 0;
-let lastPinP2 = 0;
+let lastP2 = 0;
 
 function sendData() {
     let data = {
@@ -24,29 +25,26 @@ function sendData() {
 }
 
 basic.forever(function () {
-    let osaX = input.acceleration(Dimension.X);
-    let osaY = input.acceleration(Dimension.Y);
+    let x = input.acceleration(Dimension.X);
+    let y = input.acceleration(Dimension.Y);
     let btnA = input.buttonIsPressed(Button.A);
     let btnB = input.buttonIsPressed(Button.B);
     let logo = input.logoIsPressed();
-    let pinP0 = input.pinIsPressed(TouchPin.P0);
-    let pinP1 = input.pinIsPressed(TouchPin.P1);
-    let pinP2 = input.pinIsPressed(TouchPin.P2);
+    let p2 = input.pinIsPressed(TouchPin.P2);
 
-    if (+btnA != lastBtnA || +btnB != lastBtnB || +logo != lastLogo || +pinP0 != lastPinP0 || +pinP1 != lastPinP1 || +pinP2 != lastPinP2) {
+    if (+btnA != lastBtnA || +btnB != lastBtnB || +logo != lastLogo || +p2 != lastP2) {
+        //|| x != lastX || y != lastY
         sendData();
     }
 
-    if (Math.abs(osaX) > 50 || Math.abs(osaY) > 50) {
-        sendData;
+    if (Math.abs(x) > 250 || Math.abs(y) > 250) {
+        sendData();
     }
 
-    lastOsaX = +osaX;
-    lastOsaY = +osaY;
+    lastX = x;
+    lastY = y;
     lastBtnA = +btnA;
     lastBtnB = +btnB;
     lastLogo = +logo;
-    lastPinP0 = +pinP0;
-    lastPinP1 = +pinP1;
-    lastPinP2 = +pinP2;
+    lastP2 = +p2;
 })
