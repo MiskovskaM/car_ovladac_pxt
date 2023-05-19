9fa554b8b956d7c28a7726fb7cd27e908d525c09
adyze <115144204+adamurbanek@users.noreply.github.com>
2023-05-19 17:59:35 +0200

    tak nejak to funguje!!!!!! (po třech hodinách😭😭😭😭)

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//ovladani tlacitky
+//ovladani
 radio.setGroup(54);
 radio.setFrequencyBand(7);
 
@@ -11,40 +11,47 @@ let lastP2 = 0;
 
 function sendData() {
     let data = {
-        x: input.acceleration(Dimension.X),
-        y: input.acceleration(Dimension.Y),
+        x: Math.round(input.acceleration(Dimension.X) / 1024 * 255),
+        y: Math.round(input.acceleration(Dimension.Y) / 1024 * 255),
         a: input.buttonIsPressed(Button.A),
         b: input.buttonIsPressed(Button.B),
         l: input.logoIsPressed(),
         p0: input.pinIsPressed(TouchPin.P0),
         p1: input.pinIsPressed(TouchPin.P1),
         p2: input.pinIsPressed(TouchPin.P2)
-    }
+    };
     let posliData = String.fromCharCode(data.x) + String.fromCharCode(data.y) + (data.a ? 1 : 0) + (data.b ? 1 : 0) + (data.l ? 1 : 0) + (data.p0 ? 1 : 0) + (data.p1 ? 1 : 0) + (data.p2 ? 1 : 0);
     radio.sendString(posliData);
 }
 
 basic.forever(function () {
-    let x = input.acceleration(Dimension.X);
-    let y = input.acceleration(Dimension.Y);
+    let x = input.acceleration(Dimension.X) / 1024 * 255;
+    let y = input.acceleration(Dimension.Y) / 1024 * 255;
     let btnA = input.buttonIsPressed(Button.A);
     let btnB = input.buttonIsPressed(Button.B);
     let logo = input.logoIsPressed();
     let p2 = input.pinIsPressed(TouchPin.P2);
 
-    if (+btnA != lastBtnA || +btnB != lastBtnB || +logo != lastLogo || +p2 != lastP2) {
+    if (+btnA !== lastBtnA || +btnB !== lastBtnB || +logo !== lastLogo || +p2 !== lastP2) {
         //|| x != lastX || y != lastY
         sendData();
     }
 
-    if (Math.abs(x) > 250 || Math.abs(y) > 250) {
+    if (Math.abs(x) >= 150 || Math.abs(y) >= 150) {
         sendData();
     }
 
-    lastX = x;
-    lastY = y;
+    lastX = Math.round(x);
+    lastY = Math.round(y);
     lastBtnA = +btnA;
     lastBtnB = +btnB;
     lastLogo = +logo;
     lastP2 = +p2;
 })
+
+basic.forever(function () {
+    console.logValue("X:", lastX);
+    console.logValue("Y:", lastY);
+    basic.pause(250);
+})
+
